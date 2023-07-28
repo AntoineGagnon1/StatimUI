9d100866ec5b54779bc996e2d4e7125994735525
Antoine Gagnon <antoinegagnon123@gmail.com>
2023-07-28 01:17:31 -0400

    debug tools

---
+++ StatimUI/DebugTools/DebugSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatimUI.Debug
+{
+#if DEBUG
+    public static class DebugSettings
+    {
+        public static bool ShowLayout { get; set; } // Shows the layout (padding, margin, ...) using colored sections
+
+        internal static void RenderDebugInfo(Component c, Vector2 offset)
+        {
+            if (ShowLayout)
+                Debug.ShowLayout.Render(c, offset);
+        }
+    }
+#endif
+}
