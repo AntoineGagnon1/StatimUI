c73dc22eccb2f47116fab583f15e89835f74892e
Antoine Gagnon <antoinegagnon123@gmail.com>
2023-08-27 20:26:58 -0400

    Dockspaces

--- Sandbox/Adapters/OpenGLAdapter.cs
+++ Sandbox/Adapters/OpenGLAdapter.cs
@@ -33,7 +33,7 @@ namespace Sandbox.Adapters
         private int shaderProjectionMatrixLocation;
 
         private NativeWindow mainWindow;
-        private Dictionary<Panel, SubWindow> subWindows = new ();
+        private Dictionary<Dockspace, SubWindow> subWindows = new ();
 
         private int msaaSamples;
 
@@ -56,21 +56,22 @@ namespace Sandbox.Adapters
 
             GL.Enable(EnableCap.Multisample);
 
-            subWindows.Add(window, new() { NativeWindow = mainWindow, VertexArrayObject = CreateVAO(mainWindow) }); // Add the main window to the list of subwindows, this way it will also be rendered
+            var dockspace = new Dockspace(window, new(nativeWindow.Size.X, nativeWindow.Size.Y));
+            subWindows.Add(dockspace, new() { NativeWindow = mainWindow, VertexArrayObject = CreateVAO(mainWindow) }); // Add the main window to the list of subwindows, this way it will also be rendered
         }
 
 
         // Will create a main window
-        public OpenGLAdapter(Panel window, int msaa = 4)
+        public OpenGLAdapter(Panel window, Size windowSize, int msaa = 4)
             : this(
                   window, 
                   new NativeWindow(new NativeWindowSettings()
                   {
-                    Size = new Vector2i(window.Size.Width, window.Size.Height),
+                    Size = new Vector2i(windowSize.Width, windowSize.Height),
                     WindowBorder = WindowBorder.Resizable,
                     NumberOfSamples = msaa,
                     APIVersion = OpenglGLVersion,
-                    Vsync = VSyncMode.On
+                    Vsync = VSyncMode.On,
                   }), 
                   msaa
               )
@@ -103,8 +104,7 @@ namespace Sandbox.Adapters
 
                 Renderer.ClearLayers();
                 Renderer.CurrentLayer.PushClipRect(new RectangleF(0f, 0f, pair.Key.Size.Width, pair.Key.Size.Height));
-                pair.Key.Update(); // TODO : remove ref to window when the window system changes
-                pair.Key.Render();
+                pair.Key.Update(System.Numerics.Vector2.Zero);
                 Renderer.CurrentLayer.PopClipRect();
 
                 GL.Viewport(0, 0, pair.Key.Size.Width, pair.Key.Size.Height);
@@ -122,7 +122,7 @@ namespace Sandbox.Adapters
             }
         }
 
-        private void RenderTriangles(Panel window, SubWindow subWindow)
+        private void RenderTriangles(Dockspace dockspace, SubWindow subWindow)
         {
             GL.BindVertexArray(subWindow.VertexArrayObject);
             CheckGLError();
@@ -161,8 +161,8 @@ namespace Sandbox.Adapters
 
             var pMatrix = Matrix4x4.CreateOrthographicOffCenter(
                 0.0f,
-                window.Size.Width,
-                window.Size.Height,
+                dockspace.Size.Width,
+                dockspace.Size.Height,
                 0.0f,
                 -1.0f,
                 1.0f);
@@ -190,7 +190,7 @@ namespace Sandbox.Adapters
                     GL.BufferSubData(BufferTarget.ElementArrayBuffer, IntPtr.Zero, command.Indices.Count * sizeof(uint), command.Indices.ToArray());
 
                     // 0,0 is bottom-left in opengl
-                    GL.Scissor((int)command.ClipRect.X, (int)(window.Size.Height - command.ClipRect.Height - command.ClipRect.Y), (int)command.ClipRect.Width, (int)command.ClipRect.Height);
+                    GL.Scissor((int)command.ClipRect.X, (int)(dockspace.Size.Height - command.ClipRect.Height - command.ClipRect.Y), (int)command.ClipRect.Width, (int)command.ClipRect.Height);
 
                     GL.DrawElements(BeginMode.Triangles, command.Indices.Count, DrawElementsType.UnsignedInt, 0);
                 }
@@ -305,39 +305,40 @@ namespace Sandbox.Adapters
             CheckGLError();
         }
 
-        public void CreateSubWindow(StatimUI.Panel window)
+        public void CreateSubWindow(Dockspace dockspace, Size size)
         {
             var nativeWindow = new NativeWindow(new NativeWindowSettings()
             {
                 SharedContext = mainWindow.Context,
-                Size = new Vector2i(window.Size.Width, window.Size.Height),
+                Size = new Vector2i(size.Width, size.Height),
                 WindowBorder = WindowBorder.Resizable,
                 NumberOfSamples = msaaSamples,
                 APIVersion = OpenglGLVersion
             });
 
             nativeWindow.Resize += (e) => {
-                window.Size = new (e.Size.X, e.Size.Y);
+                dockspace.Resize(new(e.Size.X, e.Size.Y));
             };
 
             nativeWindow.Closing += (e) => {
-                window.TryClose();
+                dockspace.TryClose();
             };
 
-            subWindows.Add(window, new() { NativeWindow = nativeWindow, VertexArrayObject = CreateVAO(nativeWindow) });
+            dockspace.Resize(size);
+            subWindows.Add(dockspace, new() { NativeWindow = nativeWindow, VertexArrayObject = CreateVAO(nativeWindow) });
             nativeWindow.MakeCurrent();
 
             GL.Enable(EnableCap.Multisample);
         }
 
-        public void DestroySubWindow(StatimUI.Panel window)
+        public void DestroySubWindow(Dockspace dockspace)
         {
-            var subWindow = subWindows[window];
+            var subWindow = subWindows[dockspace];
 
             subWindow.NativeWindow.MakeCurrent();
             GL.DeleteVertexArray(subWindow.VertexArrayObject);
             subWindow.NativeWindow.Dispose();
-            subWindows.Remove(window);
+            subWindows.Remove(dockspace);
         }
 
         // Creates a vao for the window
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -3,6 +3,7 @@ using StatimCodeGenerator;
 using Sandbox.Adapters;
 using StatimUI;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Drawing;
 
 //StatimUI.DebugTools.DebugSettings.ShowLayout = false;
 //StatimUI.DebugTools.DebugSettings.ShowTextRect = false;
@@ -11,8 +12,11 @@ var compiler = new Compiler();
 compiler.LoadEmbedded();
 //FocusManager.FocusedComponent = statimWindow.Root;
 
-StatimUI.Rendering.Renderer.Adapter = new OpenGLAdapter(compiler.CreatePanel("Window", new(1600, 900)));
-StatimUI.Rendering.Renderer.Adapter.CreateSubWindow(compiler.CreatePanel("Window", new(400, 400)));
+StatimUI.Rendering.Renderer.Adapter = new OpenGLAdapter(compiler.CreatePanel("Window"), new Size(1600, 900));
+StatimUI.Rendering.Renderer.Adapter.CreateSubWindow(new Dockspace(compiler.CreatePanel("Window")), new (400, 400));
+
+Dockspace.Dockspaces[0].Split(SplitDirection.X, compiler.CreatePanel("MyPanel"));
+
 (StatimUI.Rendering.Renderer.Adapter as OpenGLAdapter).Start();
 
 /*namespace Sandbox;
